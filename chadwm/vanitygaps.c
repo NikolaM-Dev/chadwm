0e413ec596e86f753415998a2bdce30a0d64b5a1
Juan David Merchan Torres <juan1014166@gmail.com>
2022-01-30 09:20:10 -0500

    feat(chadwm): Add doubledeck layout

--- chadwm/vanitygaps.c
+++ chadwm/vanitygaps.c
@@ -452,6 +452,34 @@ void deck(Monitor *m)
 		}
 }
 
+// --------------------------------------- Double Deck + gaps --------------------------------------- //
+// https://dwm.suckless.org/patches/deck/
+
+void doubledeck(Monitor *m)
+{
+	unsigned int i, n, mw;
+	Client *c;
+
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++)
+		;
+	if (n == 0)
+		return;
+
+	if (n > m->nmaster)
+		mw = m->nmaster ? m->ww * m->mfact : 0;
+	else
+		mw = m->ww;
+
+	if (n - m->nmaster > 0) // override layout symbol
+		snprintf(m->ltsymbol, sizeof m->ltsymbol, "D %d", n - m->nmaster);
+
+	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		if (i < m->nmaster)
+			resize(c, m->wx, m->wy, mw - (2 * c->bw), m->wh - (2 * c->bw), c->bw);
+		else
+			resize(c, m->wx + mw, m->wy, m->ww - mw - (2 * c->bw), m->wh - (2 * c->bw), c->bw);
+}
+
 // ------------------------------------ Fibonacci layout + gaps ------------------------------------- //
 // https://dwm.suckless.org/patches/fibonacci/
 
