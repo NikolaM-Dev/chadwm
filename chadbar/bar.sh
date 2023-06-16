6ab3c9e7be10ffc09c21dbfad75cf015cd71bde1
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-16 04:55:04 -0500

    fix(chadbar): Change deprecated nerdfont icons

--- chadbar/bar.sh
+++ chadbar/bar.sh
@@ -24,7 +24,7 @@ battery() {
 	# If this script doesn't work look at the output of upower -e and upower -d
 	get_capacity="$(cat /sys/class/power_supply/BAT0/capacity)"
 
-	printf "^c$blue^ ﴞ %s" "$get_capacity"
+	printf "^c$blue^ 󰠠 %s" "$get_capacity"
 }
 
 brightness() {
@@ -36,16 +36,16 @@ volume() {
 	is_mute=$(pamixer --get-mute)
 
 	if [ "$is_mute" = "true" ]; then
-		printf "^c$green^  %s" "$vol"
+		printf "^c$green^ 󰎊 %s" "$vol"
 	else
-		printf "^c$green^  %s" "$vol"
+		printf "^c$green^ 󰎆 %s" "$vol"
 	fi
 }
 
 cpu() {
 	cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
-	printf "^c$blue^ ﴮ %s" "$cpu_val"
+	printf "^c$blue^ 󰠰 %s" "$cpu_val"
 }
 
 mem() {
@@ -60,7 +60,7 @@ wlan() {
 }
 
 clock() {
-	printf "^c$blue^󱑆 %s" "$(date '+%I:%M %p ╷ %d.%m.%y') "
+	printf "^c$blue^ %s" "$(date '+%I:%M %p ╷ %d.%m.%y') "
 }
 
 while true; do
